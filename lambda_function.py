0b40f6da4de8d461f9e640e214550373c1de31e3
Tim <t1mtimmy@live.com>
2019-02-14 18:36:04 -0500

    First commit in deployLambda branch

--- lambda_function.py
+++ lambda_function.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 from datetime import time
 import requests
 
-def main():
+def lambda_handler(event, context):
     
     bot_token = "" # Telegram bot token.
     chat_id = "" # Chat channel you want the bot to talk in.
@@ -31,12 +31,12 @@ def main():
 
         user = sp.current_user()
 
-        count = 0
+        # count = 0 Debug
         data = {}
         
         page = sp.current_user_followed_artists()
         for artist in page["artists"]["items"]:
-            count += 1
+            # count += 1
             data.setdefault(str(artist["name"]), artist["uri"])
 
         nextCursor = page["artists"]["cursors"]["after"]
@@ -45,7 +45,7 @@ def main():
             page = sp.current_user_followed_artists(after=nextCursor)
 
             for artist in page["artists"]["items"]:
-                count += 1
+                # count += 1
                 data.setdefault(str(artist["name"]), artist["uri"])
 
             nextCursor = page["artists"]["cursors"]["after"]
@@ -71,7 +71,7 @@ def main():
                     todaysReleases.setdefault(artist, [])
                     todaysReleases[artist].append({"albumName": album["name"], "albumURL": album["external_urls"]["spotify"]})
 
-        #print("Done determining releases.") # Maybe keep just for debugging
+        # print("Done determining releases.") # Maybe keep just for debugging
 
         message = "Hello! Here's today's updates: \n\n"
 
@@ -80,7 +80,7 @@ def main():
             artistString += artist + "\n" + "\n"
 
             for album in todaysReleases[artist]:
-                print(album)
+                # print(album)
                 
                 artistString += album["albumName"] + "\n"
                 artistString += album["albumURL"] + "\n" + "\n"
@@ -94,7 +94,8 @@ def main():
 
     else:
         print("Invalid token.")
+        
+    return 'Completed'
 
 
-if __name__ == "__main__":
-    main()
+
